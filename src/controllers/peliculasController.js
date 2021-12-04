d7c66544ef96e79354778e01b1a96f9f1db9171c
llccss11 <quintana.lucas@gmail.com>
2021-12-04 11:44:15 -0300

    var entorno

--- src/controllers/peliculasController.js
+++ src/controllers/peliculasController.js
@@ -1,8 +1,11 @@
 const models = require("../models");
+const mongoose = require("mongoose");
+
+const objectIdValidator = mongoose.types.ObjectId;
 
 const getPeliculas = async (req, res) => {
     try {
-        const response = await models.Peliculas.find();
+        const response = await models.Peliculas.find();       
 
         return res.status(200).json(
             {
@@ -19,6 +22,40 @@ const getPeliculas = async (req, res) => {
         ); 
     }
 };
+
+const getPeliculaById = async (req, res) => {
+    try {
+
+        const peliculaId = req.params.id;
+        const isValid = objectIdValidator.isValid(PeliculaId);
+        if (!isValid) {
+            res.status(500).json({
+                data: `El valor ${peliculaId} no es un ID válido de MongoDB`,
+                error: true,
+              });
+        }
+      
+        const response = await models.Peliculas.findById(peliculaId);
+    
+        if (response) {
+            res.status(200).json({
+            data: response,
+            error: false,
+            });
+        } else {
+            res.status(404).json({
+            msg: "La película no existe",
+            error: true,
+            });
+        }
+        } catch (error) {
+        return res.status(500).json({
+            msg: error,
+            error: true,
+        });
+    }
+};
+  
 
 const deletePeliculas = () => console.log("delete peliculas");
 
